05357d5427b39c754d32ad053834dff9d9988c8a
Chris Bell <cjbell1988@gmail.com>
2014-06-14 17:04:09 +0100

    Separated concerns of cart vs product listing , removed App component accordingly

--- src/cart.jsx
+++ src/cart.jsx
@@ -2,15 +2,44 @@
 
 var React = require("react");
 var CartItems = require("./cart-items");
+var CartManager = require("./cart-manager");
+
+var cartManager = new CartManager();
 
 var Cart = React.createClass({
+
+  getInitialState: function() {
+    return {
+      items: cartManager.items,
+      total: cartManager.total,
+      totalItems: cartManager.totalItems
+    }
+  },
+
+  componentDidMount: function() {
+    this.props.mediator.on("cart:add", this.addToCart);
+  },
+
+  componentWillUnmount: function() {
+    this.props.mediator.off("cart:add");
+  },
+
+  addToCart: function(product) {
+    cartManager.addItem(product);
+
+    this.setState({
+      items: cartManager.items,
+      total: cartManager.total,
+      totalItems: cartManager.totalItems
+    });
+  },
 
   render: function() {
     return (
       <div className="cart">
-        <span className="cart__summary">You have {this.props.itemsInCart} items in your cart</span><br />
-        <span className="cart__total">Total: £{this.props.cartTotal.toFixed(2)}</span>
-        <div className="cart__items">{<CartItems items={this.props.cartItems} />}</div>
+        <span className="cart__summary">You have {this.state.totalItems} items in your cart</span><br />
+        <span className="cart__total">Total: £{this.state.total.toFixed(2)}</span>
+        <div className="cart__items">{<CartItems items={this.state.items} />}</div>
       </div>
     );
   }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 /** @jsx React.DOM */
 
 var React = require("react");
-var App = require("./app");
+var ProductsList = require("./products-list");
+var Cart = require("./cart");
+
+var merge = require("react/lib/merge");
+var EventEmitter = require("events").EventEmitter;
+
+var mediator = merge({}, EventEmitter.prototype);
 
 var products = [
   { id: 1, name: "Test #1", price: 19.99 },
@@ -12,5 +18,9 @@ var products = [
   { id: 6, name: "Test #6", price: 69.99 },
 ];
 
-React.renderComponent(<App products={products} />,
-                      document.getElementById("content"));
+React.renderComponent(<Cart mediator={mediator} />,
+                      document.getElementById("site-header"));
+
+React.renderComponent(<ProductsList products={products}
+                                    mediator={mediator} />,
+                      document.getElementById("site-content"));
